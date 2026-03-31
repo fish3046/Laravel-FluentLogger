fcf4808cbabd7e7e9f790717e490eb7be7b01eca
Karl Pierce <kpierce@ratespecial.com>
2026-03-31 15:44:19 -0700

    Use orchestral/testbench for testing Laravel features

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,67 +4,23 @@ declare(strict_types=1);
 
 namespace Tests;
 
-use Illuminate\Config\Repository;
-use Illuminate\Container\Container;
-use Illuminate\Contracts\Filesystem\FileNotFoundException;
-use Illuminate\Contracts\Foundation\Application;
-use Illuminate\Events\EventServiceProvider;
 use Illuminate\Filesystem\Filesystem;
-use Illuminate\Log\Context\ContextServiceProvider;
-use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+use Ytake\LaravelFluent\LogServiceProvider;
 
-use function assert;
-
-class TestCase extends PHPUnitTestCase
+class TestCase extends OrchestraTestCase
 {
-    /** @var Container  */
-    protected $app;
-
-    /**
-     * @throws FileNotFoundException
-     */
-    protected function setUp(): void
+    protected function getPackageProviders($app): array
     {
-        $this->app = $this->createApplicationContainer();
+        return [
+            LogServiceProvider::class,
+        ];
     }
 
-    /**
-     * @throws FileNotFoundException
-     */
-    protected function createApplicationContainer(): Container
+    protected function defineEnvironment($app): void
     {
-        $container = $this->getExtendedContainer();
-        assert($container instanceof Container || $container instanceof Application);
         $filesystem = new Filesystem();
-        $container->instance('config', new Repository());
-        $container['config']
-            ->set('fluent', $filesystem->getRequire(__DIR__ . '/config/fluent.php'));
-        $container['config']
-            ->set('logging', $filesystem->getRequire(__DIR__ . '/config/logging.php'));
-
-        $eventProvider = new EventServiceProvider($container);
-        $eventProvider->register();
-
-        // ContextServiceProvider was added in Laravel 11 and is a base service provider in Application.  In Laravel 12,
-        // they added ContextLogProcessor which is added to every Logger instance.  See LogManager::get()
-        $contextProvider = new ContextServiceProvider($container);
-        $contextProvider->register();
-
-        return $container;
-    }
-
-    protected function getExtendedContainer(): Container
-    {
-        return new class () extends Container {
-            public function storagePath(): string
-            {
-                return __DIR__ . '/storages';
-            }
-
-            public function runningUnitTests(): bool
-            {
-                return true;
-            }
-        };
+        $app['config']->set('fluent', $filesystem->getRequire(__DIR__ . '/config/fluent.php'));
+        $app['config']->set('logging', $filesystem->getRequire(__DIR__ . '/config/logging.php'));
     }
 }
